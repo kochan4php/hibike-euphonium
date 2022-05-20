afa2b4f3c575d8bc1a1a6eb6fa4afc5a319d4b02
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-05-21 02:32:53 +0700

    update: change to next.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    container: {
-      center: true,
-      padding: 1,
-    },
     extend: {},
   },
   plugins: [],
