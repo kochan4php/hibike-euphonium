fbb17f5898ce9148fb60d4a2979fb4d1bfd9fde1
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-05-29 12:28:00 +0700

    update: menambahkan alt pada img

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -42,7 +42,12 @@ const Navbar = () => {
     <nav className="bg-transparent z-[999] fixed w-full transition-all duration-300 text-white border-b border-b-transparent">
       <div className="container flex justify-between items-center py-1 relative">
         <div>
-          <img src="/icon/logo-hibike-euphonium.png" width="130" height="50" />
+          <img
+            src="/icon/logo-hibike-euphonium.png"
+            width="130"
+            height="50"
+            alt="logo"
+          />
         </div>
 
         <ul className="flex md:w-[60%] lg:w-[50%] absolute sm:static sm:flex-row sm:bg-transparent sm:justify-evenly md:justify-between lg:justify-evenly text-lg font-semibold right-0 flex-col bg-slate-800 bg-opacity-30 sm:bg-opacity-0 sm:backdrop-blur-none backdrop-blur-xl min-h-screen sm:min-h-0 top-0 justify-evenly items-center -z-[1] sm:z-[99] w-[73%] translate-x-[100%] sm:translate-x-0 transition-all duration-300">
